fa43383aa211c86bd243f7ae2dc0bb1316780a8c
Samuel Young <syoung5@harding.edu>
2021-02-22 21:02:51 -0600

    Create get-products.js

---
+++ functions/get-products.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch');
+
+module.exports = () => {
+  async function getProducts() {
+    const result = await fetch(
+      'https://moral-gnat-75.hasura.app/v1/graphql',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: `
+            query MyQuery {
+              products {
+                amount
+                currency
+                description
+                id
+                image
+                name
+              }
+          `,
+          variables: {},
+        }),
+      }
+    ).then((res) => res.json());
+
+    return result.data.products;
+  }
+
+  return getProducts();
+};
