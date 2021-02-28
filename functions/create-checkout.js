890e18376b5d8f1ffb6e53768e661a31c659e781
syoung6 <sam.young@mailbox.org>
2021-02-28 15:58:05 -0600

    testing out netlify functions

---
+++ functions/create-checkout.js
@@ -0,0 +1,8 @@
+import fetch from "node-fetch";
+
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: 'Create-Checkout'
+  }
+}
--- src/js/stripe-purchase.js
+++ src/js/stripe-purchase.js
@@ -1,14 +1,17 @@
 import fetch from "node-fetch";
 
-export async function handleFormSubmit(e) {
+export default async function handleFormSubmit(e) {
   e.preventDefault();
 
   const form = new FormData(e.target);
+  console.log(form);
 
   const data = {
     id: form.get('id'),
     quantity: form.get('quantity')
   };
+
+  console.log(data);
 
   const response = await fetch('/.netlify/functions/create-checkout', {
     method: 'POST',
@@ -18,12 +21,14 @@ export async function handleFormSubmit(e) {
     body: JSON.stringify(data)
   }).then(res => res.json());
 
-  const stripe = Stripe(response.publishableKey);
-  const { error } = await Stripe.redirectToCheckout({
-    sessionId: response.sessionId,
-  });
+  console.log(response);
 
-  if (error) {
-    console.error(error);
-  }
+  // const stripe = Stripe(response.publishableKey);
+  // const { error } = await stripe.redirectToCheckout({
+  //   sessionId: response.sessionId,
+  // });
+
+  // if (error) {
+  //   console.error(error);
+  // }
 }
