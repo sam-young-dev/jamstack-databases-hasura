40b5e16d4390b22c8cc2d7825774ff9547321e78
syoung6 <sam.young@mailbox.org>
2021-03-01 22:47:31 -0600

    update styling and layout structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
+// Filters
+const currencyFilter = require('./src/filters/currency-filter.js');
+
 module.exports = (config) => {
+  config.addFilter('currencyFilter', currencyFilter);
+
   config.addPassthroughCopy('src/css');
   config.addPassthroughCopy('src/images');
   config.addPassthroughCopy('src/js');
