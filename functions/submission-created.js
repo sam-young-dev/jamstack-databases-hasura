c211020fbfbe4ca5979e307d5398e8de8411b3ca
syoung6 <sam.young@mailbox.org>
2021-03-20 15:27:22 -0500

    Update submission-created.js

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,8 @@
 exports.handler = async (event) => {
   console.log(event);
+  console.log(event.payload);
+  console.log(event.payload[0].data);
+  console.log(event.payload.data);
 
   return {
     statusCode: 200,
